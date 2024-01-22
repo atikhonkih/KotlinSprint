b0e91961adbd90731892c2287e3a2bcd9851781c
anton <telcontar28@mail.ru>
2024-01-22 09:58:30 +0700

    KS-2-4 Calculate percent of buff

---
+++ src/main/kotlin/lesson_1/lesson2_task4.kt
@@ -0,0 +1,10 @@
+package lesson_1
+
+fun main() {
+    val percentOfBuff = 20
+    val crystals = 7
+    val iron = 11
+
+    println("Crystals: ${(crystals * percentOfBuff) / 100}")
+    println("Iron: ${(iron * percentOfBuff) / 100}")
+}
