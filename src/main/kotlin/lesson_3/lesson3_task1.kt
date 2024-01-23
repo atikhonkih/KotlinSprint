879d4958e1dac341f328fb66db6efeec5392772c
anton <telcontar28@mail.ru>
2024-01-23 20:21:19 +0700

    KS-3-1 Greetings user

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+    val userName = "Anonymous"
+
+    var greetings = "Good morning, $userName"
+    println(greetings)
+
+    greetings = "Good evening, $userName"
+    println(greetings)
+
+}
