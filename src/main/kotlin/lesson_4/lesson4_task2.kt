8f4f36a80ffbddd81f12c31218692c7b02ad9a5f
anton <telcontar28@mail.ru>
2024-01-27 13:02:53 +0700

    KS-4-3 Create growth conditions

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,19 +1,19 @@
 package lesson_4
 
-fun main() {
-    val minWeight = 35
-    val maxWeight = 100
-    val volume = 100
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val VOLUME = 100
 
+fun main() {
     val cargoWeight1 = 20
     val cargoVolume1 = 80
     println("Груз с весом $cargoWeight1 кг и объемом $cargoVolume1 л соответствует " +
-            "категории 'Average': ${cargoWeight1 >= minWeight && cargoWeight1 <= maxWeight 
-                    && cargoVolume1 < volume}")
+            "категории 'Average': ${cargoWeight1 >= MIN_WEIGHT && cargoWeight1 <= MAX_WEIGHT 
+                    && cargoVolume1 < VOLUME}")
 
     val cargoWeight2 = 50
     val cargoVolume2 = 100
     println("Груз с весом $cargoWeight2 кг и объемом $cargoVolume2 л соответствует " +
-            "категории 'Average': ${cargoWeight2 >= minWeight && cargoWeight2 <= maxWeight
-                    && cargoVolume2 < volume}")
+            "категории 'Average': ${cargoWeight2 >= MIN_WEIGHT && cargoWeight2 <= MAX_WEIGHT
+                    && cargoVolume2 < VOLUME}")
 }
